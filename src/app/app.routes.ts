d5c09d368f2d6807d479e42c89c4937b863d1254
Olga <olga.padkovenka.simplon@gmail.com>
2022-10-18 09:55:34 +0200

    fix problem with name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import{Routes} from "@angular/router";
 import { DescriptionComponent } from "./marketplace/description/description.component";
 import { ListNftComponent } from "./marketplace/list-nft/list-nft.component";
+import {LogGuard} from "./user/log.guard";
 
 export const ROUTES: Routes = [
     {path: '', component: ListNftComponent},
-    {path: 'description/:blockchain/:contractId/:token', component: DescriptionComponent}
+    {path: 'description/:blockchain/:contractId/:token', component: DescriptionComponent, canActivate: [LogGuard]}
 ]
